f992b5d6171f0c2f47811006d08f881868662fb8
Johannes Rabauer <j.rabauer@xdev-software.de>
2022-10-14 13:23:34 +0200

    Simple tests

--- src/main/java/software/xdev/rabauer/jpa/CustomerDao.java
+++ src/main/java/software/xdev/rabauer/jpa/CustomerDao.java
@@ -1,6 +1,11 @@
 package software.xdev.rabauer.jpa;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.util.List;
 
@@ -16,9 +21,71 @@ public class CustomerDao
 		em.getTransaction().commit();
 	}
 
-	public List<Customer> doGet()
+	public List<Customer> doGetWithJPQL()
 	{
 		EntityManager em = HibernateUtil.getSessionFactory().createEntityManager();
-		return (List<Customer>)em.createQuery("SELECT c FROM Customer c").getResultList();
+		return em.createQuery("SELECT c FROM Customer c").getResultList();
+	}
+
+	public List<Customer> doGetWithCriteria()
+	{
+		EntityManager em = HibernateUtil.getSessionFactory().createEntityManager();
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		CriteriaQuery   criteriaQuery   = criteriaBuilder.createQuery();
+		Root            customer        = criteriaQuery.from(Customer.class);
+		Query           query           = em.createQuery(criteriaQuery);
+		return query.getResultList();
+	}
+
+	public List<Customer> doGetWithMinimumAge(int minAge)
+	{
+		EntityManager em = HibernateUtil.getSessionFactory().createEntityManager();
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+
+		CriteriaQuery criteriaQuery = criteriaBuilder.createQuery();
+		Root customer = criteriaQuery.from(Customer.class);
+		criteriaQuery.where(criteriaBuilder.greaterThanOrEqualTo(customer.get("age"), minAge));
+		Query query = em.createQuery(criteriaQuery);
+		return query.getResultList();
+	}
+
+	public List<Customer> doGetWithMinimumAgeTyped(int minAge)
+	{
+		EntityManager em = HibernateUtil.getSessionFactory().createEntityManager();
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+
+		final CriteriaQuery<Customer> criteriaQuery = criteriaBuilder.createQuery(Customer.class);
+		final Root<Customer>          rootCustomer  = criteriaQuery.from(Customer.class);
+		criteriaQuery.where(criteriaBuilder.greaterThanOrEqualTo(rootCustomer.get(software.xdev.rabauer.jpa.Customer_.age), minAge));
+		final TypedQuery<Customer> typedQuery    = em.createQuery(criteriaQuery);
+		return typedQuery.getResultList();
+	}
+
+	public List<String> doGetFirstNames()
+	{
+		EntityManager em = HibernateUtil.getSessionFactory().createEntityManager();
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+
+		CriteriaQuery   criteriaQuery   = criteriaBuilder.createQuery();
+		Root<Customer>  customer        = criteriaQuery.from(Customer.class);
+		criteriaQuery.select(customer.get("firstName"));
+		Query           query           = em.createQuery(criteriaQuery);
+		return query.getResultList();
+	}
+
+	public List<Object[]> doGetGroupWithLastName()
+	{
+		EntityManager em = HibernateUtil.getSessionFactory().createEntityManager();
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+
+		final CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+		final Root<Customer>          rootCustomer  = criteriaQuery.from(Customer.class);
+		criteriaQuery.groupBy(rootCustomer.get(software.xdev.rabauer.jpa.Customer_.lastName));
+		criteriaQuery.multiselect(
+			rootCustomer.get(software.xdev.rabauer.jpa.Customer_.lastName),
+			criteriaBuilder.avg(rootCustomer.get(software.xdev.rabauer.jpa.Customer_.age))
+		);
+		TypedQuery<Object[]> typedQuery = em.createQuery(criteriaQuery);
+		return typedQuery.getResultList();
 	}
 }
--- src/test/java/software/xdev/rabauer/jpa/CustomerDaoTest.java
+++ src/test/java/software/xdev/rabauer/jpa/CustomerDaoTest.java
@@ -2,35 +2,121 @@ package software.xdev.rabauer.jpa;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class CustomerDaoTest
+class CustomerDaoTest
 {
 	@AfterEach
-	public void clearDB()
+	void clearDB()
 	{
 		HibernateUtil.shutdown();
 	}
 
 	@Test
-	public void test_inserting_simple_customer()
+	void test_inserting_simple_customer()
 	{
 		Customer c = new Customer("Tester", "Joe", 10);
-		new CustomerDao().doPut(c);
+		Assertions.assertDoesNotThrow(() -> new CustomerDao().doPut(c));
 	}
 
 	@Test
-	public void test_inserting_and_reading_simple_customer()
+	void test_inserting_and_reading_simple_customer_with_JPQL()
 	{
 		Customer c = new Customer("Tester", "Joe", 10);
 		CustomerDao customerDao = new CustomerDao();
 		customerDao.doPut(c);
 
-		List<Customer> customers = customerDao.doGet();
+		List<Customer> customers = customerDao.doGetWithJPQL();
 		Assertions.assertEquals(1, customers.size());
 		Assertions.assertEquals(c, customers.get(0));
+	}
+
+	@Test
+	void test_inserting_and_reading_simple_customer_with_criteria()
+	{
+		Customer c = new Customer("Tester", "Joe", 10);
+		CustomerDao customerDao = new CustomerDao();
+		customerDao.doPut(c);
+
+		List<Customer> customers = customerDao.doGetWithCriteria();
+		Assertions.assertEquals(1, customers.size());
+		Assertions.assertEquals(c, customers.get(0));
+	}
+
+	@Test
+	void test_queriing_custer_with_min_age()
+	{
+		Customer c1 = new Customer("Joe",  "Tester", 10);
+		Customer c2 = new Customer("Jyll", "Tester", 40);
+		Customer c3 = new Customer("Jack", "Tester", 50);
+		CustomerDao customerDao = new CustomerDao();
+		customerDao.doPut(c1);
+		customerDao.doPut(c2);
+		customerDao.doPut(c3);
+
+		List<Customer> customers = customerDao.doGetWithMinimumAge(40);
+		Assertions.assertEquals(2, customers.size());
+		Assertions.assertFalse(customers.contains(c1));
+		Assertions.assertTrue (customers.contains(c2));
+		Assertions.assertTrue (customers.contains(c3));
+	}
+
+	@Test
+	void test_queriing_custer_with_min_age_typed()
+	{
+		Customer c1 = new Customer("Joe",  "Tester", 10);
+		Customer c2 = new Customer("Jyll", "Tester", 40);
+		Customer c3 = new Customer("Jack", "Tester", 50);
+		CustomerDao customerDao = new CustomerDao();
+		customerDao.doPut(c1);
+		customerDao.doPut(c2);
+		customerDao.doPut(c3);
+
+		List<Customer> customers = customerDao.doGetWithMinimumAgeTyped(40);
+		Assertions.assertEquals(2, customers.size());
+		Assertions.assertFalse(customers.contains(c1));
+		Assertions.assertTrue (customers.contains(c2));
+		Assertions.assertTrue (customers.contains(c3));
+	}
+
+	@Test
+	void test_queriing_customer_first_names()
+	{
+		Customer c1 = new Customer("Joe",  "Tester", 10);
+		Customer c2 = new Customer("Jyll", "Tester", 40);
+		Customer c3 = new Customer("Jack", "Tester", 50);
+		CustomerDao customerDao = new CustomerDao();
+		customerDao.doPut(c1);
+		customerDao.doPut(c2);
+		customerDao.doPut(c3);
+
+		List<String> customerFirstNames = customerDao.doGetFirstNames();
+		Assertions.assertEquals(3, customerFirstNames.size());
+		Assertions.assertAll(
+			() -> Assertions.assertTrue(customerFirstNames.contains("Joe")),
+			() -> Assertions.assertTrue(customerFirstNames.contains("Jyll")),
+			() -> Assertions.assertTrue(customerFirstNames.contains("Jack"))
+		);
+	}
+
+	@Test
+	void test_queriing_group()
+	{
+		Customer c1 = new Customer("Joe",  "Tester", 10);
+		Customer c2 = new Customer("Jyll", "Tester", 40);
+		Customer c3 = new Customer("Jack", "Testerich", 50);
+		CustomerDao customerDao = new CustomerDao();
+		customerDao.doPut(c1);
+		customerDao.doPut(c2);
+		customerDao.doPut(c3);
+
+		List<Object[]> customerFirstNames = customerDao.doGetGroupWithLastName();
+		Assertions.assertEquals(2, customerFirstNames.size());
+		Assertions.assertEquals("Tester", customerFirstNames.get(0)[0]);
+		Assertions.assertEquals(25.0, customerFirstNames.get(0)[1]);
+		Assertions.assertEquals("Testerich", customerFirstNames.get(1)[0]);
+		Assertions.assertEquals(50.0, customerFirstNames.get(1)[1]);
 	}
 }
